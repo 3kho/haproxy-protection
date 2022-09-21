614b4376670c27f3223edcdac98171937fbedfd3
Thomas Lynch <tom@69420.me>
2022-09-21 21:31:48 +1000

    Add ability to include IPs in challenge hash generation, to lock cookies to IPs (like the pre-sig mode)

--- src/libs/utils.lua
+++ src/libs/utils.lua
@@ -2,12 +2,19 @@ local _M = {}
 
 local sha = require("sha")
 local secret_bucket_duration = tonumber(os.getenv("BUCKET_DURATION"))
+local challenge_includes_ip = os.getenv("CHALLENGE_INCLUDES_IP")
 
 function _M.generate_secret(context, salt, user_key, is_applet)
 
 	-- time bucket for expiry
 	local start_sec = core.now()['sec']
 	local bucket = start_sec - (start_sec % secret_bucket_duration)
+
+	-- optional IP to lock challenges/user_keys to IP (for clearnet or single-onion aka 99% of cases)
+	local ip = ""
+	if challenge_includes_ip then
+		ip = context.sf:src()
+	end
 
 	-- user agent to counter very dumb spammers
 	local user_agent = ""
@@ -19,7 +26,7 @@ function _M.generate_secret(context, salt, user_key, is_applet)
 		user_agent = context.sf:req_fhdr('user-agent') or ""
 	end
 
-	return sha.sha256(salt .. bucket .. user_key .. user_agent)
+	return sha.sha256(salt .. bucket .. ip .. user_key .. user_agent)
 
 end
 
